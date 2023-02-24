6b2be45f8e55810272fd829330649a80e3024760
Shalitha Suranga <shalithasuranga@gmail.com>
2023-02-24 15:50:37 +0530

    Add the project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { loremIpsum } from 'lorem-ipsum';
+
 import './App.css';
 
+function Row({ item }) {
+  return (
+    <div key={item.id} className="row">
+      <div className="image">
+        <img src={item.image} alt="" />
+      </div>
+      <div className="content">
+        <div>{item.name}</div>
+        <div>{item.text}</div>
+      </div>
+    </div>
+  );
+}
+
 function App() {
+  const rowCount = 5000;
+  const list = Array(rowCount).fill().map((val, idx) => {
+    return {
+      id: idx,
+      name: 'John Doe',
+      image: 'http://via.placeholder.com/40',
+      text: loremIpsum({
+        count: 1,
+        units: 'sentences',
+        sentenceLowerBound: 4,
+        sentenceUpperBound: 8
+      })
+    }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="list">
+        {list.map((item, idx) => <Row item={item} key={idx}/>)}
+      </div>
     </div>
   );
 }
